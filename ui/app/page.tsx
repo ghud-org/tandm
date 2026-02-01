f619c5213f21e0cdb22711543873a2e70c526d84
Carl (Cid) Romero <carlcidromero@gmail.com>
2026-02-01 07:34:08 -0800

    feat: tandm event creation with nextjs

---
+++ ui/app/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { Fragment, useEffect, useState } from "react";
+
+interface Tandm {
+  id: string;
+  name: string;
+  date: string;
+  amount: number;
+}
+
+export default function Home() {
+  const [tandms, setTandms] = useState<Tandm[]>([
+    {
+      id: "id",
+      name: "name",
+      date: new Date("2/1/2026").getDate().toLocaleString(),
+      amount: 0,
+    },
+    {
+      id: "id",
+      name: "name",
+      date: new Date("2/1/2026").getDate().toLocaleString(),
+      amount: 5,
+    },
+  ]);
+  const [total, setTotal] = useState<number>(0);
+
+  const updateTotal = () => {
+    let result = 0;
+    tandms.forEach((tandm) => {
+      result += tandm.amount;
+    });
+    setTotal(result);
+  };
+
+  const createTandm = () => {
+    setTandms([
+      ...tandms,
+      {
+        id: "created",
+        name: "created",
+        date: new Date("3/1/2026").getDate().toLocaleString(),
+        amount: 10,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    updateTotal();
+  }, [tandms.length]);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="border-2 border-amber-600">
+        <span>Total: {total}</span>
+      </div>
+      <button className="border-2 border-cyan-400" onClick={createTandm}>
+        Create Tandm
+      </button>
+      <ul>
+        {tandms.map((tandm) => {
+          return (
+            <div key={tandm.id} className="border-2 border-amber-300">
+              <li>Name: {tandm.name}</li>
+              <li>Date: {tandm.date}</li>
+              <li>Amount: {tandm.amount}</li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
